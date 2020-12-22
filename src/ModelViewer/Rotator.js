4aa0f756a1565e4f277eda12877f1e6fbcecd44e
NoahTN <ntolentino-nguyen@csumb.edu>
2020-12-22 12:09:16 -0800

    Add model switching options

--- src/ModelViewer/Rotator.js
+++ src/ModelViewer/Rotator.js
@@ -3,6 +3,14 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 class Rotator extends Component {
+   constructor() {
+      super();
+      this.state = {
+         name: "model"
+      };
+      this.onChangeValue = this.onChangeValue.bind(this);
+   }
+
    componentDidMount() {
       this.scene = new THREE.Scene();
       this.camera = new THREE.PerspectiveCamera( 75, 1/*window.innerWidth/window.innerHeight*/, 0.1, 1000 );
@@ -12,12 +20,24 @@ class Rotator extends Component {
       this.mount.appendChild(this.renderer.domElement);
       this.controls = new OrbitControls( this.camera, this.renderer.domElement );
       this.controls.target.set( 0, 0, 0 )
-      const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-      const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-      const line = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), new THREE.LineBasicMaterial({color: 0x00000}))
-      this.scene.add(line)
-      this.cube = new THREE.Mesh( geometry, material );
-      this.scene.add( this.cube );
+      
+      this.currentIdx = 1;
+      this.material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+      this.geoms = [
+         new THREE.SphereGeometry( 1 ), 
+         new THREE.BoxGeometry( 1, 1, 1 ),  
+         new THREE.CylinderGeometry( 1, 1, 1 )
+      ];
+      this.meshes = []
+      this.lines = []
+      for(let i = 0; i < 3; i++) {
+         this.meshes[i] = new THREE.Mesh( this.geoms[i], this.material );
+         this.lines.push(new THREE.LineSegments(new THREE.EdgesGeometry(this.geoms[i]), new THREE.LineBasicMaterial({color: 0x00000})))
+         this.meshes[i].visible = i==1 ? true : false;
+         this.lines[i].visible = i==1 ? true : false;
+         this.scene.add(this.meshes[i]);
+         this.scene.add(this.lines[i]);
+      }
       
       this.start();
    }
@@ -47,11 +67,27 @@ class Rotator extends Component {
       this.renderer.render(this.scene, this.camera)
    }
 
+   onChangeValue(event) {
+      const newIdx = parseInt(event.target.value);
+      this.meshes[this.currentIdx].visible = false;
+      this.lines[this.currentIdx].visible = false;
+      this.meshes[newIdx].visible = true;
+      this.lines[newIdx].visible = true;
+      this.currentIdx = newIdx;
+      
+   }
+
    render() {
       return (
-         <div id="model-rotator"
-            ref={ (mount) => { this.mount = mount }}
-         />
+         <div id="rotator">
+            <div id="rotator-canvas" ref={ (mount) => { this.mount = mount }}>
+            </div>
+            <div id="rotator-settings" onChange={this.onChangeValue}>
+               <input type="radio" value="0" name="Model"/>Sphere
+               <input type="radio" value="1" name="Model" defaultChecked/>Cube
+               <input type="radio" value="2" name="Model"/>Cylinder
+            </div>
+         </div>
       )
    }
 }
